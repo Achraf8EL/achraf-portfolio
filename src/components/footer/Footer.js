064d0f8274fae690239dc4f41c451605b321d858
Achraf El Messaoudi <achraf.el-messaoudi1@etu.univ-lorraine.fr>
2025-03-17 11:31:41 +0100

    Use 'isDark' to conditionally style the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,17 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Footer.scss";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
+
   return (
     <Fade bottom duration={1000} distance="5px">
-      
+      <footer className={isDark ? "footer dark" : "footer light"}>
+        {/* Your footer content here */}
+        <p>Footer Content</p>
+      </footer>
     </Fade>
   );
 }
